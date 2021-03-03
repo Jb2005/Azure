edebec1607a55bc603d2192794c947ef1e4cf1ee
Justine <Jb2005@users.noreply.github.com>
2021-03-02 19:16:13 -0500

    Play time

---
+++ Terraform/MyTurn/main.tf
@@ -0,0 +1,34 @@
+
+# Time for me to see what I can create bahahaha :)
+
+# Connect to provider and setting the version
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+# Athenticating with Azure
+provider "azurerm" {
+  features {}
+}
+
+# Creating a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "GroupForThings"
+  location = "eastus2"
+
+}
+
+# The above works lets build on it!
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnetforfun"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
---
+++ Terraform/MyTurn/terraform.tfvars
@@ -0,0 +1,4 @@
+
+# .tfvars informs terraforms that this file contains variables
+location       = "eastus2"
+prefix         = "tf"
