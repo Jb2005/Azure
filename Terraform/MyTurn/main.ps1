febbf83228728ff89612a06820bcee7618bfabfc
Justine <Jb2005@users.noreply.github.com>
2021-03-20 11:02:17 -0400

    Attempting to add a file on the new VM
    I was able to create the VM. But not move the file over. I think I need to add a connection block.
    
    A strange thing that happened was the main.tf built the resources. But when I went to destroy it told me the password value was not correct? WTF I went into the portal and removed them.

---
+++ Terraform/MyTurn/main.ps1
@@ -0,0 +1,8 @@
+
+function Main{
+
+    Write-Host "yes it worked"
+    Exit
+    
+}
+Main
--- Terraform/MyTurn/main.tf
+++ Terraform/MyTurn/main.tf
@@ -27,27 +27,69 @@ resource "azurerm_resource_group" "rg" {
 # The above works lets build on it!
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ToConnectThemAll"
+  name                = "Linux-network"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  tags                = var.Devtags
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "PublicIPForLB"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+resource "azurerm_subnet" "snet" {
+  name                 = "internal"
+  resource_group_name  = "DELETE_this_GROUP"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_lb" "example" {
-  name                = "BlanceTheTraffic"
+resource "azurerm_network_interface" "nic" {
+  name                = "mosya-nic"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vmmosya" {
+  name                = "mosya"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_password = var.adminpass
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+   # Copy in the bash script we want to execute.
+  # The source is the location of the bash script
+  # on the local linux box you are executing terraform
+  # from.  The destination is on the new AWS instance.
+  provisioner "file" {
+    source      = "C:/Users/simpl/OneDrive/Documents/GitHub/Cloud/Terraform/MyTurn/main.ps1"
+    destination = "/home/main.ps1"
+
+    connection {
+    type     = "winrm"
+    user     = "Administrator"
+    password = var.adminpass
+    host     = var.host
+  }
   }
 }
